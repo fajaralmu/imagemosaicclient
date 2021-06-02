b996dee7043d91734452b45bb98951fc65010103
Fajar AM <somabangsa@gmail.com>
2021-06-02 18:53:10 +0700

    add zoom result

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -2,7 +2,7 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import Hero from './../models/Hero'; 
 import { ImageprocessService } from './../imageprocess.service'; 
 import { doItLater } from './../util/EventUtil';
-
+const DEFAULT_RESULT_WIDTH:number = 600;
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
@@ -14,11 +14,21 @@ export class IndexComponent implements OnInit {
   sampleHero: Hero = new Hero();
   imageData?: string;
   imageDataResult?: string;
+  resultWidth:number = DEFAULT_RESULT_WIDTH;
   constructor(private service: ImageprocessService ) {
     
   } 
 
   ngOnInit(): void { }
+
+  zoomResult(factor:number) {
+    if (factor == 0) {
+      this.resultWidth = DEFAULT_RESULT_WIDTH;
+      return;
+    }
+    if (this.resultWidth+factor <0) {return;}
+    this.resultWidth+=factor;
+  }
   changeImage(e: Event): void {
     this.toBase64(e.target as HTMLInputElement)
       .then((res) => {
@@ -47,6 +57,7 @@ export class IndexComponent implements OnInit {
     this.imageDataResult = undefined;
   }
   preSubmit(): void { 
+    this.resultWidth = DEFAULT_RESULT_WIDTH;
     this.imageDataResult = undefined;
   }
   toBase64(fileInput: HTMLInputElement): Promise<any> {
